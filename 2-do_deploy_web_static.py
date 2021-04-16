c9056b903821fec5dda0ad1c64aafa47b35bf637
laurendobratz00 <laurendobratz@icloud.com>
2021-04-16 01:24:12 +0000

    add all changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 web_static folder """
 from fabric.api import *
 from datetime import datetime
-env.hosts = ['<IP web-01>', 'IP web-02']
+env.hosts = ['34.73.89.226', '18.212.230.41']
 
 
 def do_pack():
@@ -21,20 +21,19 @@ def do_deploy(archive_path):
     """ distributes an archive to your web servers, using the function do_deploy """
     if not archive_path:
         return False
-    else:
-        put('versions/web_static_20170315003959.tgz', '/tmp/web_static_20170315003959.tgz')
-        run ('mkdir -p /data/web_static/releases/web_static_20170315003959/')
-        run ('tar -xzf /tmp/web_static_20170315003959.tgz -C /data/web_static/releases/web_static_20170315003959/')
-        run ('rm /tmp/web_static_20170315003959.tgz')
-        run ('mv /data/web_static/releases/web_static_20170315003959/web_static/* /data/web_static/releases/web_static_20170315003959/')
-        run ('rm -rf /data/web_static/releases/web_static_20170315003959/web_static')
+    split_path = archive_path.split("/")[-1]
+    filename = '/data/web_static/releases/' + "{}".format(split_path.split('.')[0])
+    tmp = "/tmp/" + split_path
+
+    try:
+        put(archive_path, "/tmp/")
+        run ('mkdir -p {}/'.format(filename))
+        run ('tar -xzf {} -C {}/'.format(tmp, filename))
+        run ('rm {}'.format(tmp))
+        run ('mv {}/web_static/* {}/'.format(filename, filename))
+        run ('rm -rf {}/web_static'.format(filename))
         run ('rm -rf /data/web_static/current')
-        run ('ln -s /data/web_static/releases/web_static_20170315003959/ /data/web_static/current')
-        put ('versions/web_static_20170315003959.tgz', '/tmp/web_static_20170315003959.tgz')
-        run ('mkdir -p /data/web_static/releases/web_static_20170315003959/')
-        run ('tar -xzf /tmp/web_static_20170315003959.tgz -C /data/web_static/releases/web_static_20170315003959/')
-        run ('rm /tmp/web_static_20170315003959.tgz')
-        run ('mv /data/web_static/releases/web_static_20170315003959/web_static/* /data/web_static/releases/web_static_20170315003959/')
-        run ('rm -rf /data/web_static/releases/web_static_20170315003959/web_static')
-        run ('rm -rf /data/web_static/current')
-        run ('ln -s /data/web_static/releases/web_static_20170315003959/ /data/web_static/current')
+        run ('ln -s {}/ /data/web_static/current'.format(filename))
+        return True
+    except:
+        return False
