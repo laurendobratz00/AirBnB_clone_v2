88ae58ebf57400c7b19aff9ddda8b369ba648de7
laurendobratz00 <laurendobratz@icloud.com>
2021-04-16 01:28:41 +0000

    pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,23 +17,25 @@ def do_pack():
     else:
         return None
 
+
 def do_deploy(archive_path):
-    """ distributes an archive to your web servers, using the function do_deploy """
+    """ distributes an archive to web servers, using the function do_deploy """
     if not archive_path:
         return False
     split_path = archive_path.split("/")[-1]
-    filename = '/data/web_static/releases/' + "{}".format(split_path.split('.')[0])
+    filename = '/data/web_static/releases/' + "{}".
+    format(split_path.split('.')[0])
     tmp = "/tmp/" + split_path
 
     try:
         put(archive_path, "/tmp/")
-        run ('mkdir -p {}/'.format(filename))
-        run ('tar -xzf {} -C {}/'.format(tmp, filename))
-        run ('rm {}'.format(tmp))
-        run ('mv {}/web_static/* {}/'.format(filename, filename))
-        run ('rm -rf {}/web_static'.format(filename))
-        run ('rm -rf /data/web_static/current')
-        run ('ln -s {}/ /data/web_static/current'.format(filename))
+        run('mkdir -p {}/'.format(filename))
+        run('tar -xzf {} -C {}/'.format(tmp, filename))
+        run('rm {}'.format(tmp))
+        run('mv {}/web_static/* {}/'.format(filename, filename))
+        run('rm -rf {}/web_static'.format(filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(filename))
         return True
     except:
         return False
