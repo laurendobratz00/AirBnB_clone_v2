aff42cb11d10c28930193d2f36daf670d57de505
laurendobratz00 <laurendobratz@icloud.com>
2021-04-16 01:30:24 +0000

    changed variable name

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,19 +23,18 @@ def do_deploy(archive_path):
     if not archive_path:
         return False
     split_path = archive_path.split("/")[-1]
-    filename = '/data/web_static/releases/' + "{}".
-    format(split_path.split('.')[0])
+    name = '/data/web_static/releases/' + "{}".format(split_path.split('.')[0])
     tmp = "/tmp/" + split_path
 
     try:
         put(archive_path, "/tmp/")
-        run('mkdir -p {}/'.format(filename))
-        run('tar -xzf {} -C {}/'.format(tmp, filename))
+        run('mkdir -p {}/'.format(name))
+        run('tar -xzf {} -C {}/'.format(tmp, name))
         run('rm {}'.format(tmp))
-        run('mv {}/web_static/* {}/'.format(filename, filename))
-        run('rm -rf {}/web_static'.format(filename))
+        run('mv {}/web_static/* {}/'.format(name, name))
+        run('rm -rf {}/web_static'.format(name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(filename))
+        run('ln -s {}/ /data/web_static/current'.format(name))
         return True
     except:
         return False
